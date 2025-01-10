00cdcd7cda3b24fbbf013add5b01f86fee6b626d
awk_sed <106952099+sudo-awk@users.noreply.github.com>
2025-01-10 10:04:49 +0800

    Create messageBox+calc.c

---
+++ messageBox+calc.c
@@ -0,0 +1,53 @@
+#include <windows.h>
+#include <stdio.h>
+
+int main(void) {
+
+     MessageBoxW(
+        NULL,
+        L"Gotcha!! yOU'VE BEEN PAWNED",
+        L"oooopsss!",
+        MB_YESNOCANCEL | MB_ICONEXCLAMATION
+    );
+    /* return EXIT_SUCCESS;
+ */
+/*     BOOL CreateProcessW(
+  [in, optional]      LPCWSTR               lpApplicationName,
+  [in, out, optional] LPWSTR                lpCommandLine,
+  [in, optional]      LPSECURITY_ATTRIBUTES lpProcessAttributes,
+  [in, optional]      LPSECURITY_ATTRIBUTES lpThreadAttributes,
+  [in]                BOOL                  bInheritHandles,
+  [in]                DWORD                 dwCreationFlags,
+  [in, optional]      LPVOID                lpEnvironment,
+  [in, optional]      LPCWSTR               lpCurrentDirectory,
+  [in]                LPSTARTUPINFOW        lpStartupInfo,
+  [out]               LPPROCESS_INFORMATION lpProcessInformation
+);
+*/
+STARTUPINFOW si = { 0 };
+PROCESS_INFORMATION pi = { 0 };
+
+
+
+    if (!CreateProcessW(
+        L"C:\\Windows\\system32\\calc.exe",
+        NULL,
+        NULL,
+        NULL,
+        FALSE,
+        BELOW_NORMAL_PRIORITY_CLASS,
+        NULL,
+        NULL,
+        &si,
+        &pi
+    )
+    ){
+        printf("(-) failed to create process, error %ld", GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    printf("(+) Program started! pid: %ld", pi.dwProcessId);
+    return EXIT_SUCCESS;
+
+    
+}
